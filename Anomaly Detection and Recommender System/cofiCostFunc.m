bee9bd22997b8de98f0325abeb490630062d6d52
Moma <momir.beljic@gmail.com>
2015-04-07 19:50:50 +0300

    Calculates gradient and cost function with regularization

--- Anomaly Detection and Recommender System/cofiCostFunc.m
+++ Anomaly Detection and Recommender System/cofiCostFunc.m
@@ -1,6 +1,6 @@
 function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
                                   num_features, lambda)
-%Calculates cost function
+%Calculates cost function and gradient with regularization
 %COFICOSTFUNC Collaborative filtering cost function
 %   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
 %   num_features, lambda) returns the cost and gradient for the
